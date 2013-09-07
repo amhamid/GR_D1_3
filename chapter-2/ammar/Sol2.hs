cfd446b7148c6480a1f7bb7440da603d37848128
amhamid <amhamid@ebay.com>
2013-09-07 14:23:05 +0200

    Exercise chapter 2 till 2.9

--- chapter-2/ammar/Sol2.hs
+++ chapter-2/ammar/Sol2.hs
@@ -4,4 +4,36 @@ import GS
 import TAMO
 
 
+-- exercise 2.2
+-- P	Q	XOR
+-- t	t	 f
+-- t	f	 t
+-- f	t	 t
+-- f	f	 f
+
+
+-- exercise 2.4
+-- P 	Q	P XOR Q		P<=>Q	~(P<=>Q)
+-- t	t	   f		  t	    f
+-- t	f	   t		  f	    t
+-- f	t	   t		  f	    t
+-- f	f	   f		  t	    f
+--
+-- The implementation of XOR: x <+> y = x /= y is correct.
+
+
+-- exercise 2.9
+-- P 	Q	R = (P XOR Q)	R XOR Q 
+-- t	t	     f		   t
+-- t	f	     t		   t
+-- f	t	     t		   f
+-- f	f	     f		   f
+
+
+--exercise 2.11
+--
+--
+
+
+
 
