bdab35ea42b8576753b9015f60c45033ca771fd3
ammarhamid <amhamid@ebay.com>
2013-09-24 19:28:11 +0200

    Start with Lab 4: exercise 2

--- week4/ammar/Week4.hs
+++ week4/ammar/Week4.hs
@@ -1,5 +1,28 @@
 module Week4 where
 
 import SetOrd
+import System.Random
+import TechniquesWeek3FromLecture
 
 
+-- exercise 2
+--
+-- precondition is d >= n
+-- d is the range of random number
+-- n is how many elements in the set
+genRandomSetInt' :: Int -> Int -> Set Int -> IO (Set Int)
+genRandomSetInt' d n (Set s) | d < n = return (error "the first argument must be bigger or equal than the second argument")
+                             | length s /= n =  do x <- getRandomInt d
+                                                   Set xs <- genRandomSetInt' d n (insertSet x (Set s))
+                                                   return (Set xs)
+                             | otherwise = return (Set s)
+
+genRandomSetInt :: IO (Set Int)
+genRandomSetInt = genRandomSetInt' 100 10 emptySet
+
+
+
+-- exercise 3
+
+
+              
