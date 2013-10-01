8af5e01ac5b21c7a52fd3d746b8eca7030de9a1d
ammarhamid <amhamid@ebay.com>
2013-10-01 16:56:36 +0200

    Start Week 5 lab: exercise 1

--- week5/ammar/Week5.hs
+++ week5/ammar/Week5.hs
@@ -6,3 +6,25 @@ import Data.List
 import Week5FromLecture
 
 
+-------------------------
+-- exercise 1
+-------------------------
+
+mergeSrt ::  Ord a => [a] -> [a]
+mergeSrt [] = []
+mergeSrt (x:xs) = merge [x] (mergeSrt xs)
+
+
+mergeSrtA :: Ord a => [a] -> [a]
+mergeSrtA = invar1 sorted mergeSrt
+
+
+
+-------------------------
+-- exercise 2
+-------------------------
+
+
+
+
+
