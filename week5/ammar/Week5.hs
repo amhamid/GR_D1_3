7ce441821c8dc077dbe0e6507e3abfe36018ad4e
ammarhamid <amhamid@ebay.com>
2013-10-01 23:09:57 +0200

    continue exercise 2

--- week5/ammar/Week5.hs
+++ week5/ammar/Week5.hs
@@ -24,6 +24,28 @@ mergeSrtA = invar1 sorted mergeSrt
 -- exercise 2
 -------------------------
 
+split :: [a] -> ([a],[a])
+split xs = let
+             n = (length xs) `div` 2
+           in
+             (take n xs, drop n xs)
+
+
+mergeSrtUsingSplit :: Ord a => [a] -> [a]
+mergeSrtUsingSplit zs = let
+			   (xs, ys) = split zs
+			in
+			   merge (mergeSrt xs) (mergeSrt ys)
+
+mergeSrtUsingSplitA :: Ord a => [a] -> [a]
+mergeSrtUsingSplitA = invar1 sorted mergeSrtUsingSplit
+
+
+
+-------------------------
+-- exercise 3
+-------------------------
+
 
 
 
