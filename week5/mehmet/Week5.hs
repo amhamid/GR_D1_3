2b755732f4e2d125264e47f49aeef7aa4340c6c8
mehmetmisset <mehmet.misset@gmail.com>
2013-10-03 00:57:28 +0200

    build the new showGrid'

--- week5/mehmet/Week5.hs
+++ week5/mehmet/Week5.hs
@@ -119,4 +119,92 @@ test6_splitA = (splitA test_Array2) == test_Array5         -- False
 -- │    formalization in a program that can solve this sudoku. Deliverables:   │
 -- │    formal statement of new constraint, modified Haskell program, sudoku   │
 -- └───────────────────────────────────────────────────────────────────────────┘
+showRow1 :: [Value] -> IO()
+showRow1 [a1,a2,a3,a4,a5,a6,a7,a8,a9] = 
+ do  putChar '|'         
+     putChar ' '
+     putStr (showDgt a1)
+     putChar ' ' 
+     putChar ' ' 
+     putStr (showDgt a2)
+     putChar ' '
+     putChar ' '
+     putStr (showDgt a3)
+     putChar ' '
+     putChar '|'
+     putChar ' '
+     putStr (showDgt a4)
+     putChar ' '
+     putChar ' '
+     putStr (showDgt a5)
+     putChar ' '
+     putChar ' '
+     putStr (showDgt a6)
+     putChar ' '
+     putChar '|'
+     putChar ' '
+     putStr (showDgt a7)
+     putChar ' '
+     putChar ' '
+     putStr (showDgt a8)
+     putChar ' '
+     putChar ' '
+     putStr (showDgt a9)
+     putChar ' '
+     putChar '|'
+     putChar '\n'
+     
+showRow2 :: [Value] -> IO()
+showRow2 [a1,a2,a3,a4,a5,a6,a7,a8,a9] = 
+ do  putChar '|'         
+     putChar ' '
+     putStr (showDgt a1)
+     putChar ' ' 
+     putChar '|'
+     putStr (showDgt a2)
+     putChar ' '
+     putChar ' '
+     putStr (showDgt a3)
+     putChar ' '
+     putChar '|'
+     putChar ' '
+     putStr (showDgt a4)
+     putChar '|'
+     putChar ' '
+     putStr (showDgt a5)
+     putChar ' '
+     putChar '|'
+     putStr (showDgt a6)
+     putChar ' '
+     putChar '|'
+     putChar ' '
+     putStr (showDgt a7)
+     putChar ' '
+     putChar ' '
+     putStr (showDgt a8)
+     putChar '|'
+     putChar ' '
+     putStr (showDgt a9)
+     putChar ' '
+     putChar '|'
+     putChar '\n'
 
+showGrid' :: Grid -> IO()
+showGrid' [as,bs,cs,ds,es,fs,gs,hs,is] =
+ do putStrLn ("+---------+---------+---------+")
+    showRow1 as; 
+    putStrLn ("|   +-----|--+   +--|-----+   |")
+    showRow2 bs; 
+    showRow2 cs
+    putStrLn ("+---------+---------+---------+")
+    showRow2 ds; 
+    putStrLn ("|   +-----|--+   +--|-----+   |")
+    showRow1 es; 
+    putStrLn ("|   +-----|--+   +--|-----+   |")
+    showRow2 fs
+    putStrLn ("+---------+---------+---------+")
+    showRow2 gs; 
+    showRow2 hs; 
+    putStrLn ("|   +-----|--+   +--|-----+   |")
+    showRow1 is
+    putStrLn ("+---------+---------+---------+")
